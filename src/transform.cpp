36aaa4bf54ddd7a6bd7dc0293963d7081027a181
Aaron Liu <aaronliu0130@gmail.com>
2019-12-07 16:30:40 +0800

    Update transform.cpp
    1/8 testcases passed. Failure: stack overflow

--- src/transform.cpp
+++ src/transform.cpp
@@ -6,8 +6,9 @@ LANG: C++11
 */
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <cstring>
+#include <string>
+using namespace std;
 string rot(const string map,const int s,const int times){
     string res;
     for(int k=0;k<times;k++)
@@ -21,7 +22,12 @@ string rev(const string map,const int s){
         for(int j=0;j<s;j++)res[i*s+(s-1-j)]=map[i*s+j];
     return res;
 }
-using namespace std;
+string comb(const string map,const int s,const int times){
+	string res;
+	res=rev(res,s);
+	res=rot(res,s,times);
+	return res;	
+}
 int main() {
     ofstream fout ("transform.out");
     ifstream fin ("transform.in");
@@ -31,10 +37,13 @@ int main() {
     for(int i=0;i<s;i++)fin>>map[s*i];
     for(int i=0;i<s;i++)fin>>target[s*i];
     int res;
-    if(!strcmp(rot(map,s,1).c_str,target.c_str))res=1;
-    else if(!strcmp(rot(map,s,2).c_str,target.c_str))res=2;
-    else if(!strcmp(rot(map,s,3).c_str,target.c_str))res=3;
-    else if(!strcmp(rev(map,s).c_str,target.c_str))res=4;
+    if(!strcmp(map.c_str(),target.c_str()))res=6;
+    else if(!strcmp(rev(map,s).c_str(),target.c_str()))res=4;
+    else if(!strcmp(rot(map,s,1).c_str(),target.c_str()))res=1;
+    else if(!strcmp(rot(map,s,2).c_str(),target.c_str()))res=2;
+    else if(!strcmp(rot(map,s,3).c_str(),target.c_str()))res=3;
+    else if(!strcmp(comb(map,s,1).c_str(),target.c_str())||!strcmp(comb(map,s,2).c_str(),target.c_str())||!strcmp(comb(map,s,3).c_str(),target.c_str()))res=5;
+    else res=7;
     fout<<res<<endl;
     return 0;
 }
