ca1392833e4044f1b14ef93878380cd7a16be8c3
Aaron Liu <aaronliu0130@gmail.com>
2019-12-07 11:27:28 +0800

    Update transform.cpp
    Previous way had big memory allocation. Still work in progress.

--- transform.cpp
+++ transform.cpp
@@ -6,31 +6,34 @@ LANG: C++11
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <vector>
-#include <algorithm>
-
+#include <cstring>
+string rot(const string map,const int s,const int times){
+    string res;
+    for(int k=0;k<times;k++)
+        for(int i=0;i<s;i++)
+            for(int j=0;j<s;j++)res[(s-1-i)+j*s]=map[i*s+j];
+    return res;
+}
+string rev(const string map,const int s){
+    string res;
+    for(int i=0;i<s;i++)
+        for(int j=0;j<s;j++)res[i*s+(s-1-j)]=map[i*s+j];
+    return res;
+}
 using namespace std;
-void reverse(int &s,vector<vector<char>> &map){
-    for(int i=0;i<s;i++)
-        for(int j=0;j<s%2?s/2+1:s/2;j++)
-            swap(map[i][j],map[i][j-1-i]);
-}
-void rot(int &s,vector<vector<char>> &map){
-    vector<vector<char>> bckup=map;
-    for(int i=0;i<s;i++)
-        for(int j=0;j<s;j++){
-            map[i][j]=bckup[j][i];
-        }
-    reverse(s,map);
-}
-
 int main() {
     ofstream fout ("transform.out");
     ifstream fin ("transform.in");
     int s;
     fin>>s;
-    vector<vector<char>> map(s), target(s);
-    for(int i=0;i<s;i++)
-        for(int j=0;j<s;j++)
+    string map,target,temp;
+    for(int i=0;i<s;i++)fin>>map[s*i];
+    for(int i=0;i<s;i++)fin>>target[s*i];
+    int res;
+    if(!strcmp(rot(map,s,1).c_str,target.c_str))res=1;
+    else if(!strcmp(rot(map,s,2).c_str,target.c_str))res=2;
+    else if(!strcmp(rot(map,s,3).c_str,target.c_str))res=3;
+    else if(!strcmp(rev(map,s).c_str,target.c_str))res=4;
+    fout<<res<<endl;
     return 0;
 }
