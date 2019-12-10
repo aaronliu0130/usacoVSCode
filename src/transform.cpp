16a4f8c246c2e351c4e1715a971ed95eb9b60086
jerron <jerron@gmail.com>
2019-12-09 21:36:11 -0500

    transform

--- src/transform.cpp
+++ src/transform.cpp
@@ -1,50 +1,87 @@
 #if 1
 /*
-ID: aaronli2
+ID: jerron1
 TASK: transform
 LANG: C++11                 
 */
 #include <iostream>
 #include <fstream>
-#include <cstring>
 #include <string>
+#include <vector>
+#define PROG_ID "transform"
+
 using namespace std;
-string rot(const string map,const int s,const int times){
-    string res;
-    for(int k=0;k<times;k++)
-        for(int i=0;i<s;i++)
-            for(int j=0;j<s;j++)res[(s-1-i)+j*s]=map[i*s+j];
-    return res;
-}
-string rev(const string map,const int s){
-    string res;
-    for(int i=0;i<s;i++)
-        for(int j=0;j<s;j++)res[i*s+(s-1-j)]=map[i*s+j];
-    return res;
-}
-string comb(const string map,const int s,const int times){
-	string res;
-	res=rev(res,s);
-	res=rot(res,s,times);
-	return res;	
-}
+
+class Square{
+public:
+	Square(size_t size):sz(size),sq(size){}
+	istream& read(istream& is) {
+		for (auto i = sz - sz; i < sz; ++i)
+			is >> sq[i];
+		return is;
+	}
+	size_t size() const{return sz;}
+	static vector<string> transform(size_t op,const vector<string>&sq){
+		auto sz=sq.size();
+		vector<string> nsq(sz,string(sz,0));
+		auto proj=[&nsq,&op,&sz](int i,int j)->decltype(nsq[0][0]){
+			switch(op){
+			case 1: //90
+				return nsq[j][sz-1-i];
+			case 2: //180
+				return nsq[sz-1-i][sz-1-j];
+			case 3: //270
+				return nsq[sz-1-j][i];
+			case 4: //reflection
+				return nsq[i][sz-1-j];
+			}
+			return nsq[0][0];
+		};
+		for(auto i=sz-sz;i<sz;++i)
+			for(auto j=sz-sz;j<sz;++j)
+				proj(i,j)=sq[i][j];
+		return nsq;
+	}
+	bool equal(const Square& rhs) const{return rhs.equal(sq);}
+	bool equal(const vector<string>& rhs) const{
+		if(rhs.size()!=sz)
+			return false;
+		for(auto i=sz-sz;i<sz;++i)
+			if(sq[i]!=rhs[i])
+				return false;
+		return true;
+	}
+	int match(const Square& target){
+		vector<string> tmp(sz);
+		for(auto i=1;i<=4;++i){
+			tmp=transform(i,sq);
+			if(target.equal(tmp))
+				return i;
+		}
+		for(auto i=1;i<=3;++i)
+			if(target.equal(transform(i,tmp)))
+				return 5;
+		if(equal(target))
+			return 6;
+		return 7;
+	}
+private:
+	size_t sz;
+	vector<string> sq;
+};
+inline bool operator==(const Square& lhs, const Square& rhs){return lhs.equal(rhs);}
+inline bool operator==(const Square& lhs, const vector<string>& rhs){return lhs.equal(rhs);}
+inline istream& operator>>(istream&is,Square& sq){return sq.read(is);}
+
+
 int main() {
-    ofstream fout ("transform.out");
-    ifstream fin ("transform.in");
+    ofstream fout 	(PROG_ID".out");
+    ifstream fin 	(PROG_ID".in");
     int s;
     fin>>s;
-    string map,target,temp;
-    for(int i=0;i<s;i++)fin>>map[s*i];
-    for(int i=0;i<s;i++)fin>>target[s*i];
-    int res;
-    if(!strcmp(map.c_str(),target.c_str()))res=6;
-    else if(!strcmp(rev(map,s).c_str(),target.c_str()))res=4;
-    else if(!strcmp(rot(map,s,1).c_str(),target.c_str()))res=1;
-    else if(!strcmp(rot(map,s,2).c_str(),target.c_str()))res=2;
-    else if(!strcmp(rot(map,s,3).c_str(),target.c_str()))res=3;
-    else if(!strcmp(comb(map,s,1).c_str(),target.c_str())||!strcmp(comb(map,s,2).c_str(),target.c_str())||!strcmp(comb(map,s,3).c_str(),target.c_str()))res=5;
-    else res=7;
-    fout<<res<<endl;
+    Square source(s),target(s);
+    fin>>source>>target;
+    fout<<source.match(target)<<endl;
     return 0;
 }
 #endif
