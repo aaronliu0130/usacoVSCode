f3b181d2b8dba3f8a11e1f1d4fb284c2f062ed61
Aaron Liu <aaronliu0130@gmail.com>
2019-12-11 21:29:00 +0800

    Merge of "master" and "origin/master"
    Local and Origin: YOU GOTTA MERGE!
    Me: Okay okay shush now resolved

---
+++ src/palsquare.cpp
@@ -0,0 +1,38 @@
+/*
+ID: aaronli2
+TASK: palsquare
+LANG: C++11                 
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+using namespace std;
+string baseConv(int n,const int base){
+    string res;
+    int slasher=base;
+    while(slasher<n)slasher*=base;
+    slasher/=base;
+    while(1){
+        res+=n/slasher>=10?n/slasher-10+'A':n/slasher+'0';
+        n%=slasher;
+        if(slasher<2)break;
+        slasher/=base;
+    }
+    return res;
+}
+string rev(string str){
+    reverse(str.begin(),str.end());
+    return str;
+}
+int main() {
+    ofstream fout ("palsquare.out");
+    ifstream fin ("palsquare.in");
+    int base;
+    fin>>base;
+    for(int i=1;i<=300;i++){
+        string n=baseConv(i,base), n2=baseConv(i*i,base);
+        if(n2==rev(n2))fout<<n<<" "<<n2<<endl;
+    }
+    return 0;
+}
--- src/transform.cpp
+++ src/transform.cpp
@@ -6,45 +6,93 @@ LANG: C++11
 */
 #include <iostream>
 #include <fstream>
-#include <cstring>
 #include <string>
+#include <vector>
 using namespace std;
-string rot(const string map,const int s,const int times){
-    string res;
-    for(int k=0;k<times;k++)
-        for(int i=0;i<s;i++)
-            for(int j=0;j<s;j++)res[(s-1-i)+j*s]=map[i*s+j];
-    return res;
+class Square
+{
+public:
+    Square(size_t s) : sz(s), contents(s) {}
+    istream &read(istream &in)
+    {
+        for (auto i = 0; i < sz; ++i)
+            in >> contents[i];
+        return in;
+    }
+    size_t size() const
+    {
+        return sz;
+    }
+    static vector<string> transform(size_t operation, const vector<string> &contents)
+    {
+        auto sz = contents.size();
+        vector<string> newContents(sz, string(sz, 0));
+        auto getOp = [&newContents, &operation, &sz](int i, int j) -> decltype(newContents[0][0]) {
+            switch (operation)
+            {
+            case 1:
+                return newContents[j][sz - 1 - i];
+            case 2:
+                return newContents[sz - 1 - i][sz - 1 - j];
+            case 3:
+                return newContents[sz - 1 - j][i];
+            case 4:
+                return newContents[i][sz - 1 - j];
+            }
+            return newContents[0][0];
+        };
+        for (auto i = 0; i < sz; ++i)
+            for (auto j = 0; j < sz; ++j)
+                getOp(i, j) = contents[i][j];
+        return newContents;
+    }
+    bool equal(const vector<string> &a) const
+    {
+        if (a.size() != sz)
+            return 0;
+        for (auto i = 0; i < sz; ++i)
+            if (contents[i] != a[i])
+                return 0;
+        return 1;
+    }
+    bool equal(const Square &a) const
+    {
+        return a.equal(contents);
+    }
+    int find(const Square &target)
+    {
+        vector<string> tmp(sz);
+        for (auto i = 1; i <= 4; ++i)
+        {
+            tmp = transform(i, contents);
+            if (target.equal(tmp))
+                return i;
+        }
+        for (auto i = 1; i <= 3; ++i)
+            if (target.equal(transform(i, tmp)))
+                return 5;
+        if (equal(target))
+            return 6;
+        return 7;
+    }
+
+private:
+    size_t sz;
+    vector<string> contents;
+};
+istream &operator>>(istream &in, Square &contents)
+{
+    return contents.read(in);
 }
-string rev(const string map,const int s){
-    string res;
-    for(int i=0;i<s;i++)
-        for(int j=0;j<s;j++)res[i*s+(s-1-j)]=map[i*s+j];
-    return res;
-}
-string comb(const string map,const int s,const int times){
-	string res;
-	res=rev(res,s);
-	res=rot(res,s,times);
-	return res;	
-}
-int main() {
-    ofstream fout ("transform.out");
-    ifstream fin ("transform.in");
+int main()
+{
+    ofstream fout("transform.out");
+    ifstream fin("transform.in");
     int s;
-    fin>>s;
-    string map,target,temp;
-    for(int i=0;i<s;i++)fin>>map[s*i];
-    for(int i=0;i<s;i++)fin>>target[s*i];
-    int res;
-    if(!strcmp(map.c_str(),target.c_str()))res=6;
-    else if(!strcmp(rev(map,s).c_str(),target.c_str()))res=4;
-    else if(!strcmp(rot(map,s,1).c_str(),target.c_str()))res=1;
-    else if(!strcmp(rot(map,s,2).c_str(),target.c_str()))res=2;
-    else if(!strcmp(rot(map,s,3).c_str(),target.c_str()))res=3;
-    else if(!strcmp(comb(map,s,1).c_str(),target.c_str())||!strcmp(comb(map,s,2).c_str(),target.c_str())||!strcmp(comb(map,s,3).c_str(),target.c_str()))res=5;
-    else res=7;
-    fout<<res<<endl;
+    fin >> s;
+    Square map(s), target(s);
+    fin >> map >> target;
+    fout << map.find(target) << endl;
     return 0;
 }
 #endif
