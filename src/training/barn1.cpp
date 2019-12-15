d0ccc6766f9674736423f5302e1df823fa4c6eda
Aaron Liu <aaronliu0130@gmail.com>
2019-12-15 22:03:06 +0800

    Numerous changes
    Add problems, format problems, add header guards, add inputs, resolved barn, finished December 2019 contest

---
+++ src/training/barn1.cpp
@@ -0,0 +1,40 @@
+#if 1
+/*
+ID: aaronli2
+TASK: barn1
+LANG: C++11                 
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+bool cmp(int x, int y) { return x > y; }
+int main()
+{
+    ofstream fout("barn1.out");
+    ifstream fin("barn1.in");
+    int m, s, c;
+    fin >> m >> s >> c;
+    if (m > c)
+    {
+        printf("%d\n", c);
+        return 0;
+    }
+    int a[c + 1];
+    for (int i = 1; i <= c; i++)
+        fin >> a[i];
+    sort(a + 1, a + c + 1);
+    int ans(a[c] - a[1] + 1);
+    int d[c + 1];
+    for (int i = 2; i <= c; i++)
+        d[i - 1] = a[i] - a[i - 1];
+    sort(d + 1, d + c, cmp);
+    for (int i = 1; i <= m - 1; i++)
+        ans -= d[i] + 1;
+    fout << ans << endl;
+    return 0;
+}
+#endif
